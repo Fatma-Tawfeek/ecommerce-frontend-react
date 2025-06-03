22f8b6003f452b6c92bc0e6e3be09a243b0d47fe
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-06-03 17:14:09 +0300

    create navbar and install react-router

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
+import cart from "../assets/cart.png";
+
+const Navbar = () => {
+    const linkClass = ({ isActive }) =>
+        isActive ? "text-primary uppercase border-b-2 border-primary py-5" : "uppercase py-5";
+    return (
+        <nav className="font-raleway">
+            <div className="container">
+                <div className="flex items-center justify-between">
+                    {/* <!-- Navigation --> */}
+                    <div className="flex items-center justify-center gap-4">
+                        <NavLink to="/" className={linkClass}>
+                            All
+                        </NavLink>
+                        <NavLink to="/clothes" className={linkClass}>
+                            Clothes
+                        </NavLink>
+                        <NavLink to="/tech" className={linkClass}>
+                            Tech
+                        </NavLink>
+                    </div>
+                    {/* <!-- Logo --> */}
+                    <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
+                        <img className="h-10 w-auto" src={logo} alt="logo" />
+                    </NavLink>
+                    {/* <!-- Cart --> */}
+                    <div className="flex items-center justify-center gap-4">
+                        <button to="/cart" className={linkClass}>
+                            <img src={cart} alt="cart" className="w-6" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
